93e159a3154b84f40186e698b69f8c3efa6cff78
brancoofpa <brancoofpa@gmail.com>
2018-02-02 15:06:23 -0500

    got php code displaying user data entry on screen

--- collect.php
+++ collect.php
@@ -5,12 +5,12 @@
 	</head>
 	<body>
 		<?php
-			$username = $_GET["username"];
-			$password = $_GET["password"];
+			$username = $_POST["username"];
+			$password = $_POST["password"];
 
-			echo $username;
-			echo $password;
-			echo "did it work?"
+			echo 'username = '.$username.'</br>';
+			echo 'password = '.$password;
+	
 		?>
 	</body>
 </html>
